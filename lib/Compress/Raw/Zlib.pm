79adf8a2678fe201bac963af982754a05c2ab0f6
pmqs <pmqs@cpan.org>
2010-04-24 19:15:54 +0100

    import Compress-Raw-Zlib 2.027
      2.027 24 April 2010 
    
          * Updated to include zlib 1.2.5
    
    
    git-cpan-module:   Compress-Raw-Zlib
    git-cpan-version:  2.027
    git-cpan-authorid: pmqs

--- lib/Compress/Raw/Zlib.pm
+++ lib/Compress/Raw/Zlib.pm
@@ -13,7 +13,7 @@ use warnings ;
 use bytes ;
 our ($VERSION, $XS_VERSION, @ISA, @EXPORT, $AUTOLOAD);
 
-$VERSION = '2.026';
+$VERSION = '2.027';
 $XS_VERSION = $VERSION; 
 $VERSION = eval $VERSION;
 
