b3ca9071a901dd6fe5e9545cf759c1a656178baf
pmqs <pmqs@cpan.org>
2011-01-11 14:03:45 +0000

    import Compress-Raw-Zlib 2.033
      2.033 11 Jan 2011
          * Fixed typos & spelling errors.
            [perl# 81782]
    
    
    git-cpan-module:   Compress-Raw-Zlib
    git-cpan-version:  2.033
    git-cpan-authorid: pmqs

--- lib/Compress/Raw/Zlib.pm
+++ lib/Compress/Raw/Zlib.pm
@@ -13,7 +13,7 @@ use warnings ;
 use bytes ;
 our ($VERSION, $XS_VERSION, @ISA, @EXPORT, $AUTOLOAD);
 
-$VERSION = '2.032';
+$VERSION = '2.033';
 $XS_VERSION = $VERSION; 
 $VERSION = eval $VERSION;
 
